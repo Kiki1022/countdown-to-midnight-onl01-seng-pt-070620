effc672c54e03c977c8b140e58bfe6163ba111ae
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-01 14:39:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,51 +1,27 @@
-'require pry'
+
 def countdown(n)
-while n > 0
-puts "#{n} SECOND(S)!"
-n-=1
+  
+  while n > 0
+  
+  puts "#{n} SECOND(S)!"
+  
+  n -=1
+  
 end
-
-"HAPPY NEW YEAR!"
-
-
-end
-
-
-
-
-
-
-
-
-
-
-
-
-# #write your code here
-
-# def countdown(n)
   
-#   while n > 0
-  
-#   puts "#{n} SECOND(S)!"
-  
-#   n -=1
-  
-# end
-  
-#   "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
  
-# end 
+end 
  
 
 
-# def countdown_with_sleep(x)
-# while x > 0
-# puts "#{x} SECOND(S)!"
-#   sleep(1)
-#   x -=1
-#   end
-#   "HAPPY NEW YEAR!"
-# end
+def countdown_with_sleep(x)
+while x > 0
+puts "#{x} SECOND(S)!"
+  sleep(1)
+  x -=1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-# countdown_with_sleep(5)
+countdown_with_sleep(5)
